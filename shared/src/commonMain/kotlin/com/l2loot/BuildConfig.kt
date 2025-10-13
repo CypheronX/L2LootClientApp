caa3a545e696c5e415507c5fc900a9cbd1a148c5
Aleks <aleks.balev7899@gmail.com>
2025-10-13 02:26:30 +0200

    bumped version to 1.0.1

--- shared/src/commonMain/kotlin/com/l2loot/BuildConfig.kt
+++ shared/src/commonMain/kotlin/com/l2loot/BuildConfig.kt
@@ -5,7 +5,7 @@ package com.l2loot
  * These values are set at compile time from version.properties
  */
 object BuildConfig {
-    const val VERSION_NAME = "1.0.0"
-    const val DEBUG = true // Set to false for production builds
+    const val VERSION_NAME = "1.0.1"
+    const val DEBUG = false // Set to false for production builds
 }
 
